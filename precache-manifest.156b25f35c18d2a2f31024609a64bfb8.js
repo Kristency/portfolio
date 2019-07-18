217a6fe6e95863be1d343b0252b6e4eacca5ce25
Kristency <nandisubhajit666@gmail.com>
2019-07-18 16:34:41 +0530

    Updates

---
+++ precache-manifest.156b25f35c18d2a2f31024609a64bfb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e925bbd28047a8dda69f3e1f44cd5b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "56c085a6d65229c3cb5c",
+    "url": "/portfolio/static/css/main.7b96f59e.chunk.css"
+  },
+  {
+    "revision": "dd0886ffe429abef501b",
+    "url": "/portfolio/static/js/2.5a71347f.chunk.js"
+  },
+  {
+    "revision": "56c085a6d65229c3cb5c",
+    "url": "/portfolio/static/js/main.275d233f.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
